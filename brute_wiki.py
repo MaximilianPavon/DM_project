bce8ad04effcc5ad163f5206e4b5a3233d2f4ffe
MaximilianProll <maximilian.proll@hotmail.de>
2017-12-21 13:05:01 +0100

    further streamline code for separation of computation of LSCC and LWCC in time

--- brute_wiki.py
+++ brute_wiki.py
@@ -6,36 +6,44 @@ path_to_file = './data/wiki-Vote/wiki-Vote.txt'
 g = analysis_nx.load_graph(path_to_file, True)
 print('load graph: \t', int(divmod(time.time() - start, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
 
+print('=====LSCC=====')
+start_lscc = time.time()
 LSCC = analysis_nx.calculate_largest_strongly_connected_comp(g)
-print('calculate LSCC: \t', int(divmod(time.time() - start, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
-
-LWCC = analysis_nx.calculate_largest_weakly_connected_comp(g)
-print('calculate LWCC: \t', int(divmod(time.time() - start, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
+print('calculate LSCC: \t', int(divmod(time.time() - start_lscc, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
 
 print('LSCC edges: \t', LSCC.number_of_edges())
 print('LSCC nodes: \t', LSCC.number_of_nodes())
-print('LWCC edges: \t',LWCC.number_of_edges())
-print('LWCC nodes: \t',LWCC.number_of_nodes())
 
 LSCC_dist = analysis_nx.compute_shortest_path_distances_parallel_mp(LSCC)
-print('caluclate distances LSCC: \t', int(divmod(time.time() - start, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
-
-LWCC_dist = analysis_nx.compute_shortest_path_distances_parallel_mp(LWCC)
-print('caluclate distances LWCC: \t',int(divmod(time.time() - start, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
+print('caluclate distances LSCC: \t', int(divmod(time.time() - start_lscc, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
 
 s_median, s_mean, s_diam, s_eff_diam = analysis_nx.compute_stats(LSCC_dist)
-w_median, w_mean, w_diam, w_eff_diam = analysis_nx.compute_stats(LWCC_dist)
-
-print('=====LSCC=====')
 print('median distance: \t', s_median)
 print('mean distance: \t', s_mean)
 print('diameter: \t', s_diam)
 print('effective diameter: \t', s_eff_diam)
-print()
+
+print('LSCC done: \t', int(divmod(time.time() - start_lscc, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
+
 print('=====LWCC=====')
+start_lwcc = time.time()
+
+LWCC = analysis_nx.calculate_largest_weakly_connected_comp(g)
+print('calculate LWCC: \t', int(divmod(time.time() - start_lwcc, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
+
+print('LWCC edges: \t',LWCC.number_of_edges())
+print('LWCC nodes: \t',LWCC.number_of_nodes())
+
+LWCC_dist = analysis_nx.compute_shortest_path_distances_parallel_mp(LWCC)
+print('caluclate distances LWCC: \t',int(divmod(time.time() - start_lwcc, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
+
+w_median, w_mean, w_diam, w_eff_diam = analysis_nx.compute_stats(LWCC_dist)
+
 print('median distance: \t', w_median)
 print('mean distance: \t', w_mean)
 print('diameter: \t', w_diam)
 print('effective diameter: \t', w_eff_diam)
+
+print('LWCC done: \t', int(divmod(time.time() - start_lwcc, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
 
 print('complete code: \t', int(divmod(time.time() - start, 60)[0]), 'min:', int(divmod(time.time() - start, 60)[1]),'s')
